0f48683aa00b059f8ad08a8341e8d8b376731602
liashenkoart <liashenkoart@gmail.com>
2022-11-07 14:52:14 +0200

    fix: remove proxy

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,9 @@
 exports.createPages = async ({ actions }) => {
-  const { createPage, createRedirect } = actions
+  const { createPage } = actions
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
   })
-
-  createRedirect({
-    fromPath: `/tmep/*`,
-    toPath: `https://tmepmain.gtsb.io/tmep/*`,
-    statusCode: 200,
-  })
-
 }
